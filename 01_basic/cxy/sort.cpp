b848ff674e36b72ef08c72b9b5ce9d97ded340ae
SardineFish <Sardine@live.cn>
2017-10-31 17:29:53 +0800

    Update sort

--- 01_basic/cxy/sort.cpp
+++ 01_basic/cxy/sort.cpp
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <limits.h>
 #include "bsort.h"
 #include "isort.h"
 #include "qsort.h"
 #include "ssort.h"
 
-const long MAX_N = 100000;
+const long MAX_N = 1000000;
 int list[MAX_N];
 int count = 0;
 int main()
 {
 	char ch;
 	list[count] = 0;
-	printf("Input numbers. Input 'q' to exit.\n");
+	printf("Input numbers. Input 'q' to exit. Input 'r' to random numbers.\n");
 	while((ch = getchar()) != 'q')
 	{
+		if(ch == 'r')
+		{
+			printf("Input the number of numbers:");
+			int min,max;
+			scanf("%ld",&count);
+			printf("Min:");
+			scanf("%d",&min);
+			printf("Max:");
+			scanf("%d",&max);
+			srand(time(NULL));
+			for(long i=0;i<count;i++)
+			{
+				list[i]=rand() % (max-min)+min;
+				printf("%d ",list[i]);
+			}
+			printf("\n");
+			goto ChooseAgain;
+		}
 		if(ch == ' ' || ch == '\n')
 		{
 			count++;
@@ -47,11 +67,11 @@ ChooseAgain:
 			goto ChooseAgain;
 	}
 	end = clock();
-	printf("Sort completed in %lfs\n",(double)(end-start)/CLOCKS_PER_SEC);
+	printf("\n");
 	for(long i = 0; i < count; i++)
 	{
 		printf("%d ",list[i]);
 	}
 	printf("\n");
-
+	printf("Sort completed in %lfs\n",(double)(end-start)/CLOCKS_PER_SEC);
 }
